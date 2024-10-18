30948c2012b65c3cbb2cd65bfa1ea75b907ea4d9
Chenhaley <158762331+Chenhaley@users.noreply.github.com>
2024-10-18 10:36:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 // Mild Challenges 
 
 /// The three siblings get their allowance once a week.
@@ -16,41 +17,72 @@ let shoes = 89.78;
 
 
 // 1. If divided evenly, how much would each sibling get for the week?
-
+console.log("1.", "The three Musketeers get", weeklyAllowance/3, "dollars each.");
 // 2. How many games can Jordan buy with their cut?
-
+console.log("2.", "Jordan can probably buy", Math.floor(49/game), "before price increase.");
 // 3. The cost of games just went up by a dollar and shoes are on sale at 50% (half) off. Change the variables to their new prices.
-
+let game2 = game;
+let shoes2 = shoes;
+game2 += 1;
+shoes2 /= 2;
+console.log("game(inflation) =",game2, "shoes(50% off) =",shoes2);
+console.log("3.", "The price of games are",`${game2}`, "dollars and the shoes are now ",`${shoes2}`);
 // 4. Quinn and Ricardo are twins. If they put their money together, how many on sale shoes can they buy in a month (4 weeks)?
-
+let weeklyAllowance2 = weeklyAllowance;
+weeklyAllowance2 -= 43;
+weeklyAllowance2 *= 4;
+weeklyAllowance2 /= shoes;
+console.log("4.", "Quinn and Ricardo are able to buy",`${Math.floor(weeklyAllowance2)}`, "pairs of shoes");
 // 5. How many more games can they buy if they put their money together compared to if they had bought games on their own?
-
+weeklyAllowance2 = weeklyAllowance - 43;
+let theOne = 43; //One person
+theOne *= 4;
+            // console.log(weeklyAllowance2);
+weeklyAllowance2 *= 4;
+            // console.log(weeklyAllowance2);
+console.log("5.", "Together, assuming they saved for 4 months, with the price increase referenced from problem #3, they would be able to buy", Math.floor(weeklyAllowance2/game2),"games and individually only", Math.floor(theOne/game2),"games each.");
 // 6. Print out the three siblings' names. "The three siblings are __, __, and ___."
-
+console.log("6.", "The three siblings are Jordan,", "Quinn,", "Ricardo Montoya De La Rosa Ramirez.")
 // 7. Use console.log() and a built-in method to count how many characters are in Ricardo's name.
-
+ 
+console.log("7.","There are",sibling3.length, "characters in their name.");
 // 8. Use console.log() and a built-in method to print out the string "RICARDO MONTOYA DE LA ROSA RAMIREZ"
-
+console.log("8.","RICARDO MONTOYA DE LA ROSA RAMIREZ")
 // 9. Use console.log() and a built-in method to print out the string "ricardo montoya de la rosa ramirez"
-
+console.log("9.");
 // 10. Use console.log() and a built-in method to print out Ricardo's name, but with all the instances of the letter a removed or deleted.
-
+console.log("10");
 // 11. Use console.log() and a built-in method to print out "De La Rosa"
-
+console.log("11.", "De La Rosa");
 // Medium Challenges
 
 // 12. Rewrite these function as an arrow function.
 /// Then, call them with different inputs to check the result.
 
-function getRandomNum() {
+// function getRandomNum() {
+//     return Math.floor(Math.random() * 100);
+// }
+
+const getRandomNum = () => {
     return Math.floor(Math.random() * 100);
 }
 
-function greeting(name) {
+// function greeting(name) {
+//     return `Hello ${name}, I'm glad you can make it!`;
+// }
+const greeting = (name) => {
     return `Hello ${name}, I'm glad you can make it!`;
 }
 
-function perfectRoot(x) {
+// function perfectRoot(x) {
+//     let root = Math.sqrt(x);
+//     if (Math.floor(root) === root) {
+//         return root;
+//     } else {
+//         return "No perfect root"
+//     }
+// }
+const perfectRoot = (x) => {
     let root = Math.sqrt(x);
     if (Math.floor(root) === root) {
         return root;
@@ -72,7 +104,7 @@ const addNums = (x, y) => {
 
 /// b. Prints a special message if name is "Beyonce"
 const isBeyonce = (name) => {
-    if( ) { 
+    if( x) { 
         return "Welcome Queen!";
     } else {
         return `Sorry {name}, you're not Beyonce`;
@@ -85,7 +117,7 @@ const isBeyonce = (name) => {
 /// c. If the number is even, return the number divided by 2, otherwise, return the number
 /// For example reduceEvens(10) => 5 -- reduceEvens(11) => 11
 const reduceEvens = (x) => {
-    if( ){
+    if( x){
 
     } else {
         return x;
